bfbde15a7c5474562b3eb09e86b88dc7ecdb76d9
m <mohammedmahfoud@live.fr>
2018-11-17 15:40:45 +0100

    upload v2

--- public/theme/components/navbar.js
+++ public/theme/components/navbar.js
@@ -9,7 +9,7 @@ const navbar =
     <div class="nav-wrapper blue darken-4">
       <a href="http://localhost:8080/" class="brand-logo">&nbsp;&nbsp;PLUS ULTRA</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="../wiki/list_character.html">Personnages</a></li>
+        <li><a href="/liste">Personnages</a></li>
         <li><a href="badges.html">Components</a></li>
         <!-- Dropdown Trigger -->
         <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
