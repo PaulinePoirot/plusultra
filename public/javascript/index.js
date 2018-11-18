56f1d8e0f41f6a5ee9a7ccbf2e6f108b9ab283df
Pauline Poirot <rivaille.cass@gmail.com>
2018-11-18 19:25:06 +0100

    quote footer dynamic json

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -10,39 +10,27 @@ Vue.component("footer-bar", {
     el: '#app',
     random: 0,
     quote: "",
-    quotes: ['"All men are not created equal." ━ Izuku Midoriya',
-    '"Maybe I failed this time, but I’m not giving up." ━ Shinsou Hitoshi',
-    '"Dreams can become reality!" ━ Izuku Midoriya',
-    '"The most inflated egos are often the most fragile." ━ All Might',
-    '"Stop Talking, I will win. That’s… what heroes do." ━ Bakugou Katsuki',
-    '"Isn’t it a hero’s job to save people?" ━ Shoto Todoroki',
-    '"Heroes and villains both thrive on violence, but we’re still categorized. “You’re good” “You’re evil”." ━ Shigaraki Tomura',
-    '"The most inflated egos are often the most fragile." ━ All Might',
-    '"Whether you win or lose, looking back and learning from your experience is a part of life." ━ All Might',
+    quotes: [],
+  }),
 
-  ],
-}),
-
-methods: {
-
-  loadQuotes () {
-    axios.get('http://localhost:8080/quotes').then((response) => {
-      this.quotes = response.data
-    });
-  },
-  ready() {
-    this.random = Math.floor(Math.random() * this.quotes.length);
-    this.quote = this.quotes[this.random];
-
-    this.loadQuotes()
-    setInterval(function () {
+  methods: {
+    loadQuotes () {
+      axios.get('http://localhost:8080/quotes').then((response) => {
+        this.quotes = response.data
+        this.random = Math.floor(Math.random() * this.quotes.length);
+        this.quote = this.quotes[this.random];
+      });
+    },
+    ready() {
       this.loadQuotes()
-    }.bind(this), 30000)
+      setInterval(function () {
+        this.loadQuotes()
+      }.bind(this), 30000)
+    }
+  },
+  created() {
+    this.ready();
   }
-},
-created() {
-  this.ready();
-}
 
 });
 
@@ -198,5 +186,5 @@ let vue = new Vue({
     var urlParams = new URLSearchParams(window.location.search);
     var pseudo_perso = urlParams.get('pseudo_perso');
     this.pseudo_perso = pseudo_perso;
-}
+  }
 });
