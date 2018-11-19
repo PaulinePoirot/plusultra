ce7681c1dfb718a0819a6b6d1c0ea1958c6cb0db
m <mohammedmahfoud@live.fr>
2018-11-19 23:43:00 +0100

    src -> routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan')
 var session = require('express-session')
 var fileUpload = require('express-fileupload')
 
-var indexRouter = require('./src/index')
+var indexRouter = require('./routes/index')
 
 var app = express()
 
